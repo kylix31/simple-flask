9a3366c091893681155ef3dfeb99138e99e79de2
Raul Luzetti <raul.chiodi@gmail.com>
2024-02-07 21:05:16 -0300

    chore: added a controller midleware

--- app.py
+++ app.py
@@ -1,33 +1,24 @@
 from typing import Any
 
-from flask import Flask, request, jsonify
+from flask import Flask
 
-from flask_server.models.number_model import NumberModel
+from flask_server.controller.number_controller import NumberController
 
 app = Flask(__name__)
-model = NumberModel()
+
+controller = NumberController()
 
 
 @app.route('/add_number', methods=['POST'])
 def add_number() -> Any:
-    number = request.json['number']
-
-    if not isinstance(number, int):
-        return jsonify(error='The input is not a valid integer.'), 400
-
-    model.add_number(number)
-    return jsonify(success=True)
+    return controller.add_number()
 
 
 @app.route('/get_number/<int:number>', methods=['GET'])
 def get_number(number: int) -> Any:
-    value = model.get_value(number)
-    if value is not None:
-        return jsonify(number=number, value=value)
-
-    return jsonify(error="Number not found"), 404
+    return controller.get_number(number)
 
 
 @app.route('/get_all', methods=['GET'])
 def get_all() -> Any:
-    return jsonify(model.get_all())
+    return controller.get_all()
--- flask_server/controller/number_controller.py
+++ flask_server/controller/number_controller.py
@@ -1,20 +1,29 @@
-from models.number_model import NumberModel
+from typing import Any
+
+from flask import jsonify, request
+
+from flask_server.models.number_model import NumberModel
 
 model = NumberModel()
 
 
-def save_number(number):
-    value = model.add_number(number)
-    return jsonify({"number": number, "value": value}), 201
+class NumberController:
 
+    def add_number(self) -> Any:
+        number = request.json['number']
 
-def get_number_value(number):
-    value = model.get_value(number)
-    if value is None:
-        abort(404, description=f"Number {number} not found.")
-    return jsonify({"number": number, "value": value})
+        if not isinstance(number, int):
+            return jsonify(error='The input is not a valid integer.'), 400
 
+        model.add_number(number)
+        return jsonify(success=True)
 
-def get_all_numbers():
-    all_numbers = model.get_all()
-    return jsonify(all_numbers)
+    def get_number(self, number: int) -> Any:
+        value = model.get_value(number)
+        if value is not None:
+            return jsonify(number=number, value=value)
+
+        return jsonify(error="Number not found"), 404
+
+    def get_all(self) -> Any:
+        return jsonify(model.get_all())
